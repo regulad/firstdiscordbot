d5daeaa0306f2008d88deec66760f628f1685a23
Parker Wahle <regulad@regulad.xyz>
2024-05-07 16:36:30 -0400

    4/26/24 - Commands

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from discord import Intents
-from discord.ext.commands import Bot
+from discord.ext.commands import Bot, Context
 from dotenv import dotenv_values
 
 # Load the configuration from the .env file
@@ -13,9 +13,25 @@ bot = Bot(
     intents=intents
 )
 
+
 # Add some commands
+@bot.hybrid_command()
+async def helloworld(ctx: Context) -> None:
+    """Says hi!"""
+    await ctx.send("Hi!")
 
 
 # Run the bot
 discord_token = config["DISCORD_TOKEN"]
+guild_id = int(config["GUILD_ID"])
+
+
+@bot.event
+async def on_ready() -> None:
+    """Tell Discord about our slash commands"""
+    guild = bot.get_guild(guild_id)
+    bot.tree.copy_global_to(guild=guild)
+    await bot.tree.sync(guild=guild)
+
+
 bot.run(discord_token)
